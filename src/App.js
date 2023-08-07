a78c7b289a5da040a4efd01c8de8ce5379275af4
Brian Ngo <brianhngo@gmail.com>
2023-08-07 14:17:19 -0700

    added template map

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import LoginPage from './Forms/LoginPage';
 import SignUpModal from './Forms/SignUpModal';
 import ForgetPassword from './Forms/ForgetPassword';
 import { Route, Routes } from 'react-router-dom';
+import Map from './Maps/Map';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/map" element={<Map />} />
       <Route path="/signup" element={<SignUpModal />} />
       <Route path="/forgetPassword" element={<ForgetPassword />} />
     </Routes>
