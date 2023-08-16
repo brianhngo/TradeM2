c155a3d631f68f6018dcf00a6ada8f46997dbd5c
APatel-AI <arpan.p.se@gmail.com>
2023-08-16 06:44:53 -0500

    changed home & nav css

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,59 +8,37 @@ import '../../public/Styles.css';
 export default function Home() {
   useEffect(() => {
     const track = document.getElementById("image-track");
-
-    const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
-
-    const handleOnUp = () => {
-      track.dataset.mouseDownAt = "0";  
-      track.dataset.prevPercentage = track.dataset.percentage;
-    }
-
-    const handleOnMove = e => {
-      if (track.dataset.mouseDownAt === "0") return;
-      
-      const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-            maxDelta = window.innerWidth / 2;
-      
-      const percentage = (mouseDelta / maxDelta) * -100,
-            nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-            nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-      
-      track.dataset.percentage = nextPercentage;
-      
-      track.animate({
-        transform: `translate(${nextPercentage}%, -50%)`
-      }, { duration: 1200, fill: "forwards" });
-      
-      for (const image of track.getElementsByClassName("image")) {
-        image.animate({
-          objectPosition: `${100 + nextPercentage}% center`
-        }, { duration: 1200, fill: "forwards" });
+  
+    let currentPercentage = 0;
+  
+    const animateImages = () => {
+      currentPercentage -= 0.2
+  
+      track.style.transform = `translate(${currentPercentage}%, -50%)`;
+  
+      const images = track.getElementsByClassName("image");
+      for (const image of images) {
+        image.style.objectPosition = `${100 + currentPercentage}% center`;
       }
-    }
-
-    /* -- Had to add extra lines for touch events -- */
-
-    window.onmousedown = e => handleOnDown(e);
-
-    window.ontouchstart = e => handleOnDown(e.touches[0]);
-
-    window.onmouseup = e => handleOnUp(e);
-
-    window.ontouchend = e => handleOnUp(e.touches[0]);
-
-    window.onmousemove = e => handleOnMove(e);
-
-    window.ontouchmove = e => handleOnMove(e.touches[0]);
+    };
+  
+    // Set up automatic animation
+    const animationInterval = setInterval(animateImages, 16); // Adjust the interval as needed
+  
+    // Clean up interval on component unmount
+    return () => {
+      clearInterval(animationInterval);
+    };
   }, []);
 
   return (
     <div className={`home min-vh-100 d-flex align-items-center justify-content-center`}>
-      <div id="image-track" data-mouse-down-at="0" data-prev-percentage="0">
+      <div id="image-track">
         <img className="image" src="https://images.unsplash.com/photo-1689600570529-0e87f50e4687?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80" draggable="false" />
         <img className="image" src="https://images.unsplash.com/photo-1525695230005-efd074980869?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cmVjeWNsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" draggable="false" />
         <img className="image" src="https://images.pexels.com/photos/2583836/pexels-photo-2583836.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" draggable="false" />
-        {/* ... Add other image elements similarly */}
+        <img className="image" src="https://images.pexels.com/photos/6280474/pexels-photo-6280474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" draggable="false" />
+       
         <div className="overlay"></div>
       </div>
 
@@ -68,7 +46,7 @@ export default function Home() {
         <Row className="justify-content-center">
           <Col xs={12} md={8} lg={6}>
             <div className="welcome-text fade-in">
-              <h1 className="display-3 ">
+              <h1 className="display-3">
                 Welcome to {" "}
                 <span
                   className="display-2"
