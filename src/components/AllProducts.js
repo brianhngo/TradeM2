76f660cff66e031d3f95a0b8b5cd5cf07a6f935d
APatel-AI <arpan.p.se@gmail.com>
2023-08-18 19:23:06 -0500

    updated products css

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { getDatabase, ref, child, get } from 'firebase/database';
-import './AllProducts.css';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import Bookmark from './Bookmark'; // Make sure you have the correct path to your Bookmark component
+import React, { useEffect, useState } from "react";
+import { getDatabase, ref, child, get } from "firebase/database";
+import "./AllProducts.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Bookmark from "./Bookmark"; // Make sure you have the correct path to your Bookmark component
 
 const AllProducts = ({ updateMapLocation }) => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [filter, setFilter] = useState({
-    category: 'All',
-    price: 'All',
+    category: "All",
+    price: "All",
   });
 
   useEffect(() => {
     const dbRef = ref(getDatabase());
-    const productsRef = child(dbRef, 'Products');
+    const productsRef = child(dbRef, "Products");
 
     get(productsRef).then((snapshot) => {
       if (snapshot.exists()) {
@@ -29,26 +29,26 @@ const AllProducts = ({ updateMapLocation }) => {
   useEffect(() => {
     let result = products;
 
-    if (filter.category !== 'All') {
+    if (filter.category !== "All") {
       result = result.filter((product) => product.category === filter.category);
     }
 
-    if (filter.price !== 'All') {
+    if (filter.price !== "All") {
       switch (filter.price) {
-        case '50':
+        case "50":
           result = result.filter((product) => product.price < 50);
           break;
-        case '50-100':
+        case "50-100":
           result = result.filter(
             (product) => product.price >= 50 && product.price <= 100
           );
           break;
-        case '100-500':
+        case "100-500":
           result = result.filter(
             (product) => product.price >= 100 && product.price <= 500
           );
           break;
-        case '500-1000':
+        case "500-1000":
           result = result.filter(
             (product) => product.price >= 500 && product.price <= 1000
           );
@@ -65,7 +65,7 @@ const AllProducts = ({ updateMapLocation }) => {
     if (location) {
       console.log("Locating product at:", location);
 
-      const [lat, lon] = location.split(',').map(parseFloat);
+      const [lat, lon] = location.split(",").map(parseFloat);
 
       // If coordinates are available
       if (!isNaN(lat) && !isNaN(lon)) {
@@ -76,7 +76,7 @@ const AllProducts = ({ updateMapLocation }) => {
         try {
           const response = await axios.get(
             `https://nominatim.openstreetmap.org/search?q=${location}&format=json`
-            );
+          );
           if (response.data && response.data.length > 0) {
             const { lat, lon } = response.data[0];
             const coordinates = [parseFloat(lat), parseFloat(lon)];
@@ -92,8 +92,6 @@ const AllProducts = ({ updateMapLocation }) => {
       console.log("Product location missing or map reference is missing");
     }
   };
-
-
 
   return (
     <div className="all-products-container">
@@ -124,47 +122,36 @@ const AllProducts = ({ updateMapLocation }) => {
           <option value="500-1000">500 to 1000</option>
         </select>
       </div>
-      <ul className="product-list">
-        {filteredProducts.map((product) => (
-          <li className="product-item" key={product.id}>
-            <Link className="product-link" to={`/singleproduct/${product.productId}`}>
-              <Bookmark productId={product.productId} />
-              <img
-                className="product-img"
-                src={product.imageUrl}
-                alt={product.ProductName}
-              />
-                        </Link>
-              <div className="product-details">
-                <p className="product-texts">Category: {product.category} </p>
-                <p className="product-texts">Name: {product.name} </p>
-                <p className="product-texts">
-                  Description: {product.description}
-                </p>
-                <p className="product-price">Price: ${product.price}</p>
-                <button onClick={() => locateProduct(product.location)}>
-                  Locate Product
-                </button>
-              </div>
-              {/* <svg
-                className="svgTest"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#000000"
-                version="1.1"
-                id="Capa_1"
-                width="100px"
-                height="100px"
-                viewBox="0 0 36.09 36.09"
-              >
-              
-                <g>
-                <path d="M36.042,13.909c-0.123-0.377-0.456-0.646-0.85-0.688l-11.549-1.172L18.96,1.43c-0.16-0.36-0.519-0.596-0.915-0.596   s-0.755,0.234-0.915,0.598L12.446,12.05L0.899,13.221c-0.394,0.04-0.728,0.312-0.85,0.688c-0.123,0.377-0.011,0.791,0.285,1.055   l8.652,7.738L6.533,34.045c-0.083,0.387,0.069,0.787,0.39,1.02c0.175,0.127,0.381,0.191,0.588,0.191   c0.173,0,0.347-0.045,0.503-0.137l10.032-5.84l10.03,5.84c0.342,0.197,0.77,0.178,1.091-0.059c0.32-0.229,0.474-0.633,0.391-1.02   l-2.453-11.344l8.653-7.737C36.052,14.699,36.165,14.285,36.042,13.909z M25.336,21.598c-0.268,0.24-0.387,0.605-0.311,0.957   l2.097,9.695l-8.574-4.99c-0.311-0.182-0.695-0.182-1.006,0l-8.576,4.99l2.097-9.695c0.076-0.352-0.043-0.717-0.311-0.957   l-7.396-6.613l9.87-1.002c0.358-0.035,0.668-0.264,0.814-0.592l4.004-9.077l4.003,9.077c0.146,0.328,0.456,0.557,0.814,0.592   l9.87,1.002L25.336,21.598z" />
-              </g>
-              </svg> */}
-  
-          </li>
-        ))}
-      </ul>
+      <div className="product-list">
+      {filteredProducts.map((product) => (
+  <div className="product-item" key={product.id}>
+      <div className="product-img-container">
+        <button className='locate-product' onClick={() => locateProduct(product.location)}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+          </svg>
+        </button>
+        <Bookmark productId={product.productId} className="bookmark-icon" /> {/* Add the Bookmark component here */}
+        <Link className="product-link" to={`/singleproduct/${product.productId}`}>
+
+        <img
+          className="product-img"
+          src={product.imageUrl}
+          alt={product.ProductName}
+        />
+        </Link>
+      </div>
+    
+    <div className="product-details">
+      <p className="product-texts">Category: {product.category} </p>
+      <p className="product-texts">Name: {product.name} </p>
+      <p className="product-texts">Description: {product.description}</p>
+      <p className="product-price">Price: ${product.price}</p>
+    </div>
+  </div>
+))}
+
+      </div>
     </div>
   );
 };
