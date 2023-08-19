ce0049581150291da253006e085d05844af23412
APatel-AI <arpan.p.se@gmail.com>
2023-08-19 18:08:14 -0500

    updated css

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -131,7 +131,7 @@ const AllProducts = ({ updateMapLocation }) => {
             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
           </svg>
         </button>
-        <Bookmark productId={product.productId} className="bookmark-icon" /> {/* Add the Bookmark component here */}
+        <Bookmark productId={product.productId} className="bookmark-icon" />
         <Link className="product-link" to={`/singleproduct/${product.productId}`}>
 
         <img
